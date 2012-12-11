d51b21667efc57cbdbd82dbf6998a1bad5ecbe8f
srikar chowdary <srikarchowdary.gl@gmail.com>
2012-12-11 21:12:19 +0000

    project

---
+++ cric_pro/app/controllers/site_controller.rb
@@ -0,0 +1,13 @@
+class SiteController < ApplicationController
+  def index
+  end
+
+  def images
+  end
+
+  def videos
+  end
+
+  def contactus
+  end
+end
---
+++ cric_pro/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project2::Application.config.secret_token = '0e569ca5a74e63612c993ee7560642a7d1a728aff02d79ebdf32463d3e662e6e8fbca65e0b83210f7e1a4ca09253505b1420e338a93e25f3a2318a1672e7060d'
